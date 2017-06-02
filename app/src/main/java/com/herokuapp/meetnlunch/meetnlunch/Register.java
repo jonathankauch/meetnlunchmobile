1664f7d663b4c5b7dca4946fe5c0694f980f6c6d
Shirco <christophe.fraysse@epitech.eu>
2017-06-02 18:29:34 +0900

    Ion and Gson libs added + register call

--- app/src/main/java/com/herokuapp/meetnlunch/meetnlunch/Register.java
+++ app/src/main/java/com/herokuapp/meetnlunch/meetnlunch/Register.java
@@ -1,12 +1,22 @@
 package com.herokuapp.meetnlunch.meetnlunch;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -34,10 +44,55 @@ public class Register extends AppCompatActivity {
 
             @Override
             public void onClick(View mLoginFormView) {
+                final JsonObject json = new JsonObject();
+                json.addProperty("email", getEmailText());
+                json.addProperty("password", getPasswordText());
+                json.addProperty("name", getNameText());
+
+                if (!isNetworkAvailable())
+                    return;
+//                showProgress(true);
+
+                Ion.with(getApplicationContext())
+                        .load(getString(R.string.api_url) + "register")
+                        .setJsonObjectBody(json)
+                        .asJsonObject()
+                        .setCallback(new FutureCallback<JsonObject>() {
+                            @Override
+                            public void onCompleted(Exception e, JsonObject result) {
+                                if (e != null || result == null) {
+                                    return;
+                                }
+                                Log.d("API RESPONSE", result.toString());
+//                                JsonElement jsonUser = result.get("user");
+//                                getUser(jsonUser.getAsJsonObject());
+//                                User user = new Gson().fromJson(jsonUser, User.class);
+//                                if (user != null) {
+//                                    Log.d("USER PARSED", "email : " + user.getEmail());
+//                                    Log.d("USER PARSED", "firstname : " + user.getFirstname());
+//                                    Log.d("USER PARSED", "lastname : " + user.getLastname());
+//                                    Log.d("USER PARSED", "token : " + user.getToken());
+//                                    Log.d("USER PARSED", "id : " + user.getId());
+//                                    Singleton.getInstance(user.getToken(), user.getId());
+//                                    Singleton.getInstance().setmUser(user);
+//                                    Intent intent = new Intent(LoginActivity.this, LeftMenuActivity.class);
+//                                    startActivity(intent);
+//                                }
+                            }
+                        });
+
                 String API_URL="https://meetnlunchapp.herokuapp.com/api/register?email=" + getEmailText() +"&password=" + getPasswordText() + "&name=" + getNameText();
             }
         });
     }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
 
     public View getProgressBar() {
         return findViewById(R.id.register_progress);
